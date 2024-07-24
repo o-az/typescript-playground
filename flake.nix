31056581821c27a470d7991dad46b215bd9e656c
omar <omaraziz.dev@proton.me>
2024-07-24 07:48:21 -0700

    first commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "tunnel localhost for a quick 'lgtm?'";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell =
+          with pkgs;
+          pkgs.mkShell {
+            buildInputs = [
+              bun
+              just
+              biome
+              direnv
+              nixfmt-rfc-style
+              nodePackages_latest.nodejs
+            ];
+          };
+      }
+    );
+}
